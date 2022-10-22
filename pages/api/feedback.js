eb1ec7ac942e7ba5e43e72cb80cb3d03b48da738
geochronology <54387644+geochronology@users.noreply.github.com>
2022-10-22 16:20:57 -0500

    added feedback handler

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,9 +1,30 @@
+import fs from 'fs';
+import path from 'path';
 /* -- API --
   the `api` directory lets you execute any server-side code;
   folder *must* be named 'api'... or else >:3
 */
 function handler(req, res) {
-  res.status(200).json({ message: 'This works' });
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text: feedbackText,
+    };
+
+    // store feedback in db / file
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: 'Success', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'This works' });
+  }
 }
 
 export default handler;
